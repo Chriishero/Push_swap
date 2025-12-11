d19f002ab724c46b8044dae95c8ffcff983618c4
Christian Villeneuve <cvillene@c4r2p1.42nice.fr>
2025-12-11 09:21:54 +0100

    insertion sorting

--- simple_sorting.c
+++ simple_sorting.c
@@ -6,37 +6,37 @@
 /*   By: cvillene <cvillene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 07:49:54 by cvillene          #+#    #+#             */
-/*   Updated: 2025/12/11 09:12:59 by cvillene         ###   ########.fr       */
+/*   Updated: 2025/12/11 09:19:05 by cvillene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	find_max_index(t_stack *a)
+static int	find_min_index(t_stack *a)
 {
 	int	i;
-	int	max_idx;
-	int	max;
+	int	min_idx;
+	int	min;
 
 	if (!a)
 		return (FAILURE);
 	i = 0;
-	max_idx = 0;
-	max = a->content;
+	min_idx = INT_MAX;
+	min = a->content;
 	while (a)
 	{
-		if (a->content > max)
+		if (a->content < min)
 		{
-			max = a->content;
-			max_idx = i;
+			min = a->content;
+			min_idx = i;
 		}
 		a = a->next;
 		i++;
 	}
-	return (max_idx);
+	return (min_idx);
 }
 
-// selection sorting
+// insertion sorting
 t_monitoring	simple_sorting(t_stack **a, t_stack **b, t_monitoring m)
 {
 	int				size;
@@ -46,7 +46,7 @@ t_monitoring	simple_sorting(t_stack **a, t_stack **b, t_monitoring m)
 	while (*a)
 	{
 		size = stack_size(*a);
-		idx = find_max_index(*a);
+		idx = find_min_index(*a);
 		if (idx == FAILURE)
 			break ;
 		if (idx <= size / 2)
@@ -59,7 +59,6 @@ t_monitoring	simple_sorting(t_stack **a, t_stack **b, t_monitoring m)
 				m.n_rra += do_rr(a, 'a');
 		}
 		m.n_pb += do_p(a, b, 'b');
-		m.n_rb += do_r(b, 'b');
 	}
 	while (*b)
 		m.n_pa += do_p(b, a, 'a');
